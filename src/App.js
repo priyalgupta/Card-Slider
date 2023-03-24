338ad93bad09fcb439d7e5f5a5c8ba725f1d314e
priyalgupta <66379600+priyalgupta@users.noreply.github.com>
2023-03-24 22:43:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Clients from "./components/Clients/Clients";
+
+function App() {
+  return (
+    <Container>
+      
+      <Clients />
+      
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div``
